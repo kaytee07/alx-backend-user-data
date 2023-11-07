c4d563589996f5c67e9c09dfcbfa410c3eb0e4e5
kaytee07 <kofi.taylor07@gmail.com>
2023-11-07 11:06:22 +0000

    unathorized access

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -14,6 +14,13 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
+@app.errorhandler(401)
+def not_found(error) -> str:
+    """ Not found handler
+    """
+    return jsonify({"error": "Unauthorized"}), 404
+
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
