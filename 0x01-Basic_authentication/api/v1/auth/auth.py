6197414868495581533bcfb37ecd10c0d96d7af7
kaytee07 <kofi.taylor07@gmail.com>
2023-11-07 11:57:52 +0000

    route that dont need authentications

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -3,6 +3,7 @@
 auth module
 """
 from flask import request
+from typing import List, TypeVar
 
 
 class Auth:
@@ -20,13 +21,26 @@ class Auth:
         
         returns: False
         """
-        return False
+        if path is None:
+            return True
 
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        normalized_path = path.rstrip('/')
+        normalized_excluded_path = [p.rstrip('/') for p in excluded_paths]
+        if normalized_path in normalized_excluded_path:
+            return False
+        else:
+            return True
+
+        return False
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
         get current user
         """
+        return None
 
 
     def authorization_header(self, request=None) -> str:
