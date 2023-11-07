5082a28dfe93182f1a2db9da5c51b913c9e15936
kaytee07 <kofi.taylor07@gmail.com>
2023-11-07 17:46:08 +0000

    push

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -21,24 +21,25 @@ class Auth:
 
         returns: False
         """
-        if path is not None and excluded_paths is not None:
-            for exclusion_path in map(lambda x: x.strip(), excluded_paths):
-                pattern = ''
-                if exclusion_path[-1] == '*':
-                    pattern = '{}.*'.format(exclusion_path[0:-1])
-                elif exclusion_path[-1] == '/':
-                    pattern = '{}/*'.format(exclusion_path[0:-1])
-                else:
-                    pattern = '{}/*'.format(exclusion_path)
-                if re.match(pattern, path):
-                    return False
-        return False
+         if path is None:
+            return True
 
-    def current_user(self, request=None) -> TypeVar('User'):
-        """
-        get current user
-        """
-        return None
+        if excluded_paths is None or excluded_paths == []:
+            return True
+
+        if path in excluded_paths:
+            return False
+
+        for excluded_path in excluded_paths:
+            if excluded_path.startswith(path):
+                return False
+            elif path.startswith(excluded_path):
+                return False
+            elif excluded_path[-1] == "*":
+                if path.startswith(excluded_path[:-1]):
+                    return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
