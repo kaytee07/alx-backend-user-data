447fc97676e490f0f431014b5bcc73a3f484d7eb
kaytee07 <kofi.taylor07@gmail.com>
2023-11-07 16:12:13 +0000

    basic authentication

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,13 +3,17 @@
 basic_auth module
 """
 from api.v1.auth.auth import Auth
+import binascii
+import base64
 
 
 class BasicAuth(Auth):
     """
     Authentication with Basic Auth scheme
     """
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(
+            self,
+            authorization_header: str) -> str:
         """
         encode authorization_header with base64
         """
@@ -24,3 +28,38 @@ class BasicAuth(Auth):
             return None
         else:
             return parts[1]
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str) -> str:
+        """
+        Decodes a base64-encoded authorization header.
+        """
+        if type(base64_authorization_header) == str:
+            try:
+                res = base64.b64decode(
+                    base64_authorization_header,
+                    validate=True,
+                )
+                return res.decode('utf-8')
+            except (binascii.Error, UnicodeDecodeError):
+                return None
+
+    def extract_user_credentials(
+            self,
+            decoded_base64_authorization_header: str) -> (str, str):
+        """
+        returns the user email and password from the
+        Base64 decoded value.
+        """
+        if decoded_base64_authorization_header is None:
+            return (None, None)
+
+        if type(decoded_base64_authorization_header) != str:
+            return (None, None)
+
+        if ':' in decoded_base64_authorization_header:
+            parts = decoded_base64_authorization_header.split(':')
+            return (parts[0], parts[1])
+        else:
+            return (None, None)
