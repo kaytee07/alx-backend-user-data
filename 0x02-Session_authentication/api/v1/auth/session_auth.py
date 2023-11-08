acc38b85ea6e8413bb2de53f5a61b82786a70928
kaytee07 <kofi.taylor07@gmail.com>
2023-11-08 16:38:46 +0000

    retrieve user id from session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,14 +3,32 @@
 session_auth module
 """
 from api.v1.auth.auth import Auth
-
+import uuid
 
 class SessionAuth(Auth):
     """
     session_authentication
     """
-    def __init__(self):
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
         """
-        initialize class
+        creates a session_id for a user
         """
-        pass
+        
+        if type(user_id) is str:
+            session_id = uuid.uuid4().hex
+            self.user_id_by_session_id[session_id] = user_id
+            return session_id
+        
+        return None
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        return a User ID based on session id
+        """
+        if type(session_id) is str:
+            user_id = self.user_id_by_session_id.get(session_id)
+            return user_id
+        return None
+
