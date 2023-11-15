e0f16e6dc2b6882b7989d2414ad1cbe205c2ea39
kaytee07 <kofi.taylor07@gmail.com>
2023-11-15 09:28:16 +0000

    end point to create new user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python3
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 @app.route("/", methods=["GET"], strict_slashes=False)
 def greetings():
     return jsonify({"message": "Bienvenue"})
 
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def register_user():
+    """
+    create new user with email and password from form   
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email":f"{user.email}",
+                         "message": "user created"})
+    except ValueError:
+        return jsonify({"message":"email already registered"}), 400
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
