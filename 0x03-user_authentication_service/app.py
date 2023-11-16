88059d344af4abf5978feb20541be9116b0782cf
kaytee07 <kofi.taylor07@gmail.com>
2023-11-16 09:33:19 +0000

    pycodestyle errors

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
- #!/usr/bin/env python3
- """
- flask-app for user authentication service
- """
+#!/usr/bin/env python3
+"""
+flask-app for user authentication service
+"""
 from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 
@@ -9,23 +9,26 @@ from auth import Auth
 app = Flask(__name__)
 AUTH = Auth()
 
+
 @app.route("/", methods=["GET"], strict_slashes=False)
 def greetings():
     return jsonify({"message": "Bienvenue"})
 
+
 @app.route("/users", methods=["POST"], strict_slashes=False)
 def register_user():
     """
-    create new user with email and password from form   
+    create new user with email and password from form
     """
     email = request.form.get('email')
     password = request.form.get('password')
     try:
         user = AUTH.register_user(email, password)
-        return jsonify({"email":f"{user.email}",
-                         "message": "user created"})
+        return jsonify({"email": f"{user.email}",
+                        "message": "user created"})
     except ValueError:
-        return jsonify({"message":"email already registered"}), 400
+        return jsonify({"message": "email already registered"}), 400
+
 
 @app.route("/sessions", methods=["POST"], strict_slashes=False)
 def login():
@@ -43,6 +46,7 @@ def login():
     else:
         abort(401)
 
+
 @app.route("/sessions", methods=["DELETE"], strict_slashes=False)
 def logout():
     """
@@ -55,6 +59,7 @@ def logout():
     AUTH.destroy_session(user.id)
     redirect("/")
 
+
 @app.route("/profile", methods=["GET"], strict_slashes=False)
 def profile():
     """
@@ -65,6 +70,7 @@ def profile():
         abort(403)
     user = AUTH.get_user_from_session_id(session_id)
     return jsonify({"email": f"{user.email}"}), 200
+
 
 @app.route("/reset_password", methods=["POST"], strict_slashes=False)
 def get_reset_password_token():
@@ -77,6 +83,7 @@ def get_reset_password_token():
     token = AUTH.get_reset_password_token(email)
     return jsonify({"email": f"{email}",
                     "reset_token": f"{token}"})
+
 
 @app.route("/reset_password", methods=["PUT"], strict_slashes=False)
 def update_password():
@@ -95,6 +102,7 @@ def update_password():
     if not is_password_changed:
         abort(403)
     return jsonify({"email": email, "message": "Password updated"})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
