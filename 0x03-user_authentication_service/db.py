461522c765047474610a407549be392ce57bf512
kaytee07 <kofi.taylor07@gmail.com>
2023-11-16 08:18:14 +0000

    correct type annotation

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,9 +5,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from user import Base, User
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
+
+
+from user import Base, User
 
 
 class DB:
